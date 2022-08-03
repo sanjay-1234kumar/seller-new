64a125358c6100305c52826a61e76f921578cc8b
sanjay chandel <sanjaychatuwa.din@gmail.com>
2022-08-03 10:46:58 +0530

    first commit

---
+++ build/precache-manifest.4fe8b9638dd927842f35951d29e75da4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35a5f24ed567785be576393400b61d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebd25dc3fe6fd94e16a8",
+    "url": "/static/js/2.af45caac.chunk.js"
+  },
+  {
+    "revision": "3ef49141adc5a809abe30374ef0f5bf0",
+    "url": "/static/js/2.af45caac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d340c48ceabc9241ef4",
+    "url": "/static/js/main.75252122.chunk.js"
+  },
+  {
+    "revision": "5820d58c60ab3b1375ad",
+    "url": "/static/js/runtime-main.99b514f4.js"
+  }
+]);
